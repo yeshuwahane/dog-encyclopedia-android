8a497156cc025b0ec96652b6e3f028399ace5a32
yeshuwahane <yeshuwahane@gmail.com>
2021-06-08 19:13:21 +0530

    inital commit

---
+++ app/src/main/java/com/example/mybio/shibu.kt
@@ -0,0 +1,13 @@
+package com.example.mybio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class shibu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shibu)
+
+
+    }
+}
